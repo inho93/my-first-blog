97c5902c9fbd7387a2094a938e537f7877f272c1
ihpark <qkrdlsgh93@gmail.com>
2019-07-17 17:05:08 +0900

    add new post

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,8 @@ from django.conf import settings
 
 
 class Post(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    #author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    author = models.CharField(max_length=30)
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
